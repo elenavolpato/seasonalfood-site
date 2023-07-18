b03db4c009292011f2df073ec8f82e6b078dca99
Elena Volpato <talkto@elenavolpato.me>
2023-07-18 15:34:49 +0200

    added app print

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: `/`,
+  server: {
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'build',
+  },
+  
+  
+})
